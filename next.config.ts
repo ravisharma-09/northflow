397ff51b1205948338aa1f9c4d587395ac942015
Ravi Sharma <sharmaravi75080@gmail.com>
2026-08-05 15:34:06 +0530

    Remove experimental turbopack config

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   compress: true,
-  experimental: {
-    turbopack: {
-      root: './'
-    }
-  },
   images: {
     formats: ['image/avif', 'image/webp'],
     minimumCacheTTL: 60,
